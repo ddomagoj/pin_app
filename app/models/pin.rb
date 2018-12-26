072dd504633710a17da1af94c12b8c80630ee45d
ddomagoj <ddespala@gmail.com>
2018-12-27 00:07:19 +0100

    Revert "added act-as-votable"
    This reverts commit 092b1585145be0c47ca3e00ddb0d1c4c2637fe81.

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,5 @@
 class Pin < ApplicationRecord
   belongs_to :user
-  acts_as_votable
 
   has_attached_file :image, styles: { medium: "500x500>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :pins do 
-    member do
-     put "like", to: "pins#upvote"
-    end
-   end
+  resources :pins
 
   root "pins#index"
 end
