428de1c23f4ae8181f8a8e2f51be7db33a525353
ddomagoj <ddespala@gmail.com>
2019-01-03 12:50:02 +0100

    ppc

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,3 @@
+Paperclip::Attachment.default_options[:url] = 'https://bucketeer-98de21ea-d77b-49ee-9c67-82758d092008.s3.amazonaws.com/public/:s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+
